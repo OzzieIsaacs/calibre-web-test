01086a056ccdb2e11b48cb5990f0e87dc8f97524
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2021-05-06 21:26:59 +0200

    update kobo sync

--- test/test_kobo_sync.py
+++ test/test_kobo_sync.py
@@ -594,3 +594,7 @@ class TestKoboSync(unittest.TestCase, ui_class):
             print(e)
             self.assertFalse(e, data)
 
+    def test_kobo_sync_selected_chelfs(self):
+        self.inital_sync()
+        # erzeuge privaten Shelf ohne sync
+        # sync
