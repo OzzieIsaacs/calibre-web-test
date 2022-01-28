cebc1f372dee68fad6066ef7fde40bcc6292d3ea
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2022-01-28 20:02:15 +0100

    Fix for dev branch

--- test/test_edit_books_metadata.py
+++ test/test_edit_books_metadata.py
@@ -158,7 +158,7 @@ class TestLoadMetadata(TestCase, ui_class):
         self.assertLessEqual(diff(BytesIO(cover), BytesIO(original_cover), delete_diff_file=True), 0.001)
         self.assertEqual(results[0]['title'], self.check_element_on_page((By.ID, "book_title")).get_attribute("value"))
         self.assertEqual(results[0]['author'], self.check_element_on_page((By.ID, "bookAuthor")).get_attribute("value"))
-        self.assertEqual("/../../../static/generic_cover.jpg", self.check_element_on_page((By.ID, "cover_url")).get_attribute("value"))
+        self.assertEqual("/static/generic_cover.jpg", self.check_element_on_page((By.ID, "cover_url")).get_attribute("value"))
 
         self.fill_basic_config({'config_uploading': 0})
         time.sleep(3)
